02a8f9fa40f17156a06c4df8d10e34d30395e0d6
Shilpashreeau <shilpashreehebbar@gmail.com>
2022-12-16 09:38:24 -0500

    Added Register.feature and Register step definition file

---
+++ src/test/java/steps/RegisterSteps.java
@@ -0,0 +1,147 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegisterSteps {
+	@Given("User on ds-algo Register page")
+	public void user_on_ds_algo_register_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks register after entering only username")
+	public void user_clicks_register_after_entering_only_username() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("It should display an error {string} below password textbox")
+	public void it_should_display_an_error_below_password_textbox(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks register after entering username & password")
+	public void user_clicks_register_after_entering_username_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("It should display an error {string} below Password Confirmation textbox")
+	public void it_should_display_an_error_below_password_confirmation_textbox(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks register after entering username, password & confirm password")
+	public void user_clicks_register_after_entering_username_password_confirm_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be redirected to Homepage with the message {string}")
+	public void the_user_should_be_redirected_to_homepage_with_the_message(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks register after entering {int} characters and password")
+	public void user_clicks_register_after_entering_characters_and_password(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Remain in the Register page")
+	public void remain_in_the_register_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks register after entering {int} characters, password and confirm password")
+	public void user_clicks_register_after_entering_characters_password_and_confirm_password(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks register leaving username and password blank")
+	public void user_clicks_register_leaving_username_and_password_blank() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("It should display an error {string} below Username textbox")
+	public void it_should_display_an_error_below_username_textbox(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks register entering only password and confirm password")
+	public void user_clicks_register_entering_only_password_and_confirm_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enters only numerics  in username field, password and confirm password")
+	public void user_enters_only_numerics_in_username_field_password_and_confirm_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enters special characters with alphanumeric ,password and confirm password")
+	public void user_enters_special_characters_with_alphanumeric_password_and_confirm_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enters special characters in username , password and confirm password")
+	public void user_enters_special_characters_in_username_password_and_confirm_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User on ds-algo Register page and enters username and clicks on the password field")
+	public void user_on_ds_algo_register_page_and_enters_username_and_clicks_on_the_password_field() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enters less than {int} characters")
+	public void user_enters_less_than_characters(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("It should display an error message\"password more than {int} characters\"")
+	public void it_should_display_an_error_message_password_more_than_characters(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enters only characters")
+	public void user_enters_only_characters() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("It should display an error message {string}")
+	public void it_should_display_an_error_message(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enters only numerics")
+	public void user_enters_only_numerics() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user enters a common password")
+	public void the_user_enters_a_common_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
---
+++ target/test-classes/features/Register.feature
@@ -0,0 +1,73 @@
+Feature: Validating register functionality
+
+	Scenario: Validating functionality of register by entering username
+		Given User on ds-algo Register page
+		When User clicks register after entering only username
+		Then It should display an error "Please fill out this field" below password textbox
+	
+	Scenario: Validating functionality of register entering username and password
+		Given User on ds-algo Register page
+		When User clicks register after entering username & password
+		Then It should display an error "Please fill out this field" below Password Confirmation textbox
+
+	Scenario: Validating functionality of register entering valid username, password, confirm password
+		Given User on ds-algo Register page
+		When User clicks register after entering username, password & confirm password
+		Then The user should be redirected to Homepage with the message "New Account Created. You are logged in as <username>"
+
+	Scenario: Validating functionality of register entering more than 150 characters
+		Given User on ds-algo Register page
+		When User clicks register after entering 151 characters and password
+		Then Remain in the Register page
+
+	Scenario: Validating functionality of register entering 149 characters
+		Given User on ds-algo Register page
+		When User clicks register after entering 149 characters, password and confirm password
+		Then The user should be redirected to Homepage with the message "New Account Created. You are logged in as <username>"
+	
+	Scenario: Validating functionality of register leaving username and password fields blank  
+		Given User on ds-algo Register page  
+		When User clicks register leaving username and password blank
+		Then It should display an error "Please fill out this field" below Username textbox
+	
+	Scenario: Validating functionality of register without entering username
+		Given User on ds-algo Register page
+		When User clicks register entering only password and confirm password
+		Then It should display an error "Please fill out this field" below Username textbox
+	
+	Scenario: Validating functionality of register entering only numeric values in username
+		Given User on ds-algo Register page
+		When User enters only numerics  in username field, password and confirm password
+		Then The user should be redirected to Homepage with the message "New Account Created. You are logged in as <username>"
+
+	Scenario: Validating functionality of register entering special characters in username
+		Given User on ds-algo Register page
+		When User enters special characters with alphanumeric ,password and confirm password
+		Then The user should be redirected to Homepage with the message "New Account Created. You are logged in as <username>"
+	
+	Scenario: Validating functionality of register entering only special characters in username
+		Given User on ds-algo Register page
+		When User enters special characters in username , password and confirm password
+		Then The user should be redirected to Homepage with the message "New Account Created. You are logged in as <username>"
+
+	Scenario: Validating functionality of password field with less than 8 characters
+		Given User on ds-algo Register page and enters username and clicks on the password field
+		When User enters less than 8 characters
+		Then It should display an error message"password more than 8 characters"
+	
+	Scenario: Validating functionality of password field with more  than 8 characters
+		Given User on ds-algo Register page and enters username and clicks on the password field
+		When User enters only characters
+		Then It should display an error message "password_mismatch:The two password fields didn’t match."
+	
+	Scenario: Validating functionality of password field with more  than 8 numerics
+		Given User on ds-algo Register page and enters username and clicks on the password field
+		When User enters only numerics 
+		Then It should display an error message "password_mismatch:The two password fields didn’t match."
+
+	Scenario: Validating functionality of password with common password
+		Given User on ds-algo Register page and enters username and clicks on the password field
+		When The user enters a common password
+		Then It should display an error message "password_mismatch:The two password fields didn’t match."
+
+  
