4324369de733740ab01b1332d0134a0e6fc567bc
Ramya Varanasi <ramya.varanasi5a3@gmail.com>
2022-12-26 22:28:34 -0500

    Added Signout Step definition

---
+++ src/test/java/steps/SignOutSteps.java
@@ -0,0 +1,20 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SignOutSteps {
+
+@Given("The user logged in and on the homepage")
+public void the_user_logged_in_and_on_the_homepage() {
+   }
+
+@When("The user clicks {string}")
+public void the_user_clicks(String string) {
+   }
+
+@Then("It should display a message {string} and goes back to portal home page")
+public void it_should_display_a_message_and_goes_back_to_portal_home_page(String string) {
+}
+}
--- target/test-classes/features/SignOut.feature
+++ target/test-classes/features/SignOut.feature
@@ -3,6 +3,7 @@ Feature: Validating Signout
 
   @tag1
   Scenario: Validating Signout
-  Given: The user logged in and on the homepage
-  When: The user clicks "Sign out" 
-  Then: It should display a message "Logged out successfully" and goes back to portal home page
+  
+  Given The user logged in and on the homepage
+  When The user clicks "Sign out" 
+  Then It should display a message "Logged out successfully" and goes back to portal home page
